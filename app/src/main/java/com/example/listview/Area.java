c646c77ec46279948a173fed0c16a2506dddd18e
GermanVq <gavegaq@gmail.com>
2020-09-28 20:38:26 -0500

    Created area class

--- app/src/main/java/com/example/listview/Area.java
+++ app/src/main/java/com/example/listview/Area.java
@@ -1,4 +1,37 @@
 package com.example.listview;
 
 public class Area {
+    private Double altura;
+    private Double base;
+    private Double lado;
+
+    public Area(Double altura, Double base, Double lado) {
+        this.altura = altura;
+        this.base = base;
+        this.lado = lado;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getBase() {
+        return base;
+    }
+
+    public void setBase(Double base) {
+        this.base = base;
+    }
+
+    public Double getLado() {
+        return lado;
+    }
+
+    public void setLado(Double lado) {
+        this.lado = lado;
+    }
 }
